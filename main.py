d17c5fad21b8319b5c684e2109fd313f9afc8d1c
ivan <ivan.shpotenko@gmail.com>
2018-03-27 15:08:04 +0300

    wip

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+from googletrans import Translator
 from textblob import Word
 import pandas as pd
 # from translate import Translator
 # translator = Translator(to_lang="uk")
 
 from mtranslate import translate
-
-df = pd.read_csv("in.csv", header=None)
-demo = df[1].dropna()
 
 
 def get_meanings(words):
@@ -42,15 +40,43 @@ def get_translations(words):
     return ret
 
 
-words = demo
-meanings = get_meanings(demo)
-translations = get_translations(demo)
+def get_google_translations(words, translator):
+    res = list()
+    num_words = len(words)
+    i = 0
+    tenth = num_words // 10
+    word_ind = 0
+    print('Starting translation...')
+    for word in words:
+        word_ind += 1
+        if word_ind % tenth == 0:
+            i += 1
+            print(f'{10*i} % of words are translated')
+        print(f'word: {word}')
+        transl = translator.translate(word, 'uk')
+        print(f'transl: {transl.text}')
+        res.append(transl.text)
 
-out = dict()
-out['words'] = words
-out['meanings'] = meanings
-out['translations'] = translations
+    return res
 
-df = pd.DataFrame.from_dict(out)
-print(df)
-df.to_csv('out.csv')
+
+if __name__ == '__main__':
+    df = pd.read_csv("in.csv", header=None)
+    demo = df[1].dropna()
+
+    translator = Translator()
+
+    words = demo
+    meanings = get_meanings(demo)
+    translations = get_translations(demo)
+    google_translations = get_google_translations(demo, translator)
+
+    out = dict()
+    out['words'] = words
+    out['meanings'] = meanings
+    out['translations'] = translations
+    out['translations_google'] = google_translations
+
+    df = pd.DataFrame.from_dict(out)
+    print(df)
+    df.to_csv('out.csv')
