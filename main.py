1f6e931eff4119dea34be87c1f89cf90acd0c1cf
AndriyLazorenko <andriy.lazorenko@gmail.com>
2018-03-27 14:59:35 +0300

    v. 0.01

--- main.py
+++ main.py
@@ -1,56 +1,76 @@
 from textblob import Word
+from textblob.wordnet import VERB
 import pandas as pd
 # from translate import Translator
 # translator = Translator(to_lang="uk")
 
 from mtranslate import translate
 
-df = pd.read_csv("in.csv", header=None)
-demo = df[1].dropna()
+
+class MeaningTranslator:
+    def __init__(self):
+        self.df = pd.read_csv("in.csv", header=None)
+        self.demo = df[1].dropna()
+
+    def get_meanings(self, words):
+        ret = list()
+        for word in words:
+            w_list = word.split()
+            if len(w_list) == 1:
+                w = Word(word)
+                try:
+                    ret.append(w.definitions[0])
+                except:
+                    ret.append('NO MEANING FOUND')
+            elif w_list[0] == 'to' and len(w_list) == 2:
+                w = Word(w_list[1])
+                try:
+                    v = w.get_synsets(pos=VERB)
+                    ret.append(v[0].definition())
+                except:
+                    ret.append('NO MEANING FOUND')
+            else:
+                ret.append('NOT IMPLEMENTED FOR 2 OR MORE WORDS')
+        return ret
+
+    def get_translations(self, words):
+        ret = list()
+        num_words = len(words)
+        i = 0
+        tenth = num_words // 10
+        word_ind = 0
+        print('Starting translation...')
+        for word in words:
+            word_ind += 1
+            if word_ind % tenth == 0:
+                i += 1
+                print(f'{10*i} % of words are translated')
+            transl = translate(word, 'uk')
+            ret.append(transl)
+
+        return ret
+
+    def form_dict(self, words, meanings, translations):
+        out = dict()
+        out['words'] = words
+        out['meanings'] = meanings
+        out['translations'] = translations
+        return out
+
+    def save_df(self, dic):
+        df = pd.DataFrame.from_dict(dic)
+        print(df)
+        df.to_csv('out.csv')
+
+    def run(self):
+        words = self.demo
+        meanings = self.get_meanings(self.demo)
+        translations = self.get_translations(self.demo)
+        out = self.form_dict(words, meanings, translations)
+        self.save_df(out)
 
 
-def get_meanings(words):
-    ret = list()
-    for word in words:
-        w_list = word.split()
-        if len(w_list) == 1:
-            w = Word(word)
-            try:
-                ret.append(w.definitions[0])
-            except:
-                ret.append('NO MEANING FOUND')
-        else:
-            ret.append('NOT IMPLEMENTED FOR 2 OR MORE WORDS')
-    return ret
+if __name__ == '__main__':
+    mt = MeaningTranslator()
+    mt.run()
 
-
-def get_translations(words):
-    ret = list()
-    num_words = len(words)
-    i = 0
-    tenth = num_words // 10
-    word_ind = 0
-    print('Starting translation...')
-    for word in words:
-        word_ind += 1
-        if word_ind % tenth == 0:
-            i += 1
-            print(f'{10*i} % of words are translated')
-        transl = translate(word, 'uk')
-        ret.append(transl)
-
-    return ret
-
-
-words = demo
-meanings = get_meanings(demo)
-translations = get_translations(demo)
-
-out = dict()
-out['words'] = words
-out['meanings'] = meanings
-out['translations'] = translations
-
-df = pd.DataFrame.from_dict(out)
-print(df)
-df.to_csv('out.csv')
--- proofs.py
+++ proofs.py
@@ -1,4 +1,9 @@
 from textblob import Word
+from textblob.wordnet import VERB
+from nltk.corpus import wordnet as wn
+
 word = Word("plant", pos_tag='v')
+v = word.get_synsets(pos=VERB)
 print(word.synsets[:5])
 print(word.definitions[0])
+print(v[0].definition())
